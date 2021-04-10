23e62bcbf7791b43fb81402e3ecc669826b3f0ad
Eric Raywood <eric.o.raywood@gmail.com>
2021-04-09 23:00:01 -0700

    move light source to top middle
    move light source to top middle

--- data/shaders/shadow.fs.glsl
+++ data/shaders/shadow.fs.glsl
@@ -12,29 +12,19 @@ void main()
 {
     color = vec4(0.0, 0.0, 0.0, 0.0);
 
-    //now we step towards (0,0)
+    //now we step towards (0.5,1)
 
-    //vec4 in_color = texture(screen_texture, texcoord);
-    //bool check_colours = in_color.a != 0.0; //only check colours if the alpha is non-zero (starting on an object)
-
-    vec2 unit_direction = normalize(vec2(-texcoord.x, 1.0 - texcoord.y));
+    vec2 unit_direction = normalize(vec2(0.5 - texcoord.x, 1.0 - texcoord.y));
     unit_direction.x *= 0.005;
     unit_direction.y *= 0.005;
     vec2 current_position = texcoord + unit_direction;
-    while (current_position.x > 0.01 && current_position.y < 0.99) 
+    while (current_position.x > 0.01 && current_position.x < 0.99 && current_position.y < 0.99) 
     {
         
         vec4 current_color = texture(screen_texture, current_position);
         if (current_color.a != 0.0) 
         {
-            //(!check_colours || current_color != in_color)
-            //if you are checking colours, and the color is the same, then it doesn't count
-
-            vec4 after_color = texture(screen_texture, current_position + unit_direction / 2);
-            if (after_color.a != 0.0)  
-            {
-                color = vec4(0.0, 0.0, 0.0, 0.2);
-            }
+            color = vec4(0.0, 0.0, 0.0, 0.2);
         }
         current_position += unit_direction;
     }
